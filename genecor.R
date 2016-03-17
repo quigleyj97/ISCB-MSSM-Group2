f6b77fd789513bdebe462f627402b2d6e51060d7
Joe Quigley <quigleyj@mssm.org>
2016-03-17 16:28:41 -0400

    Linted with lintr for readability

--- genecor.R
+++ genecor.R
@@ -24,13 +24,12 @@ load(file="BTBR.data.clean.Rdata")
 # Side Effect: Accesses variables not passed as arguments
 # returns: islet gene expression
 load_islet <- function(gene) {
-	# Initial validity checks. If any of these fail, then
-	# the preconditions are not met. See comments above.
-	stopifnot(exists(deparse(substitute(annot))))
-	gene_id = grep(gene, annot$gene1)
-	stopifnot(length(gene_id)==1)
-	
-	islet.rz[, annot[gene_id, 1]]
+  # Initial validity checks. If any of these fail, then
+  # the preconditions are not met. See comments above.
+  stopifnot(exists(deparse(substitute(annot))))
+  gene_id <- grep(gene, annot$gene1)
+  stopifnot(length(gene_id) == 1)
+  islet.rz[, annot[gene_id, 1]]
 }
 
 # Prepares the f2g cross object for QTL scans by
@@ -44,14 +43,13 @@ load_islet <- function(gene) {
 # Side Effect: Accesses variables not passed as arguments
 # returns: new f2g$pheno
 prepare_for_scan <- function(clin_pheno, gene_pheno) {
-	# Precondition checks
-	stopifnot(exists(deparse(substitute(f2g))))
-	stopifnot(all(clin_pheno %in% names(phenotypes.rz)))
-	
-	cbind(
-		f2g$pheno[,c("MouseNum","Sex","pgm")],
-		phenotypes.rz[,clin_pheno],
-		gene_pheno)
+  # Precondition checks
+  stopifnot(exists(deparse(substitute(f2g))))
+  stopifnot(all(clin_pheno %in% names(phenotypes.rz)))
+  cbind(
+    f2g$pheno[,c("MouseNum","Sex","pgm")],
+    phenotypes.rz[,clin_pheno],
+    gene_pheno)
 }
 
 # =====Plotting functions
@@ -60,41 +58,36 @@ prepare_for_scan <- function(clin_pheno, gene_pheno) {
 
 # Side Effect: modifies variables not passed as arguments
 # returns: void
-panel.cor <- function(x, y, sigfigs=2, prefix="", sig_threshold=0.5,  cex.cor, ...) {
-	# To ensure non-destructive parameter changing
-	usr <- par("usr")
-	on.exit(par(usr))
-	par(usr) <- c(0, 1, 0, 1) # Possible issue could come from an edge case here...
-	
-	r <- cor(x, y, use="complete.obs")
-	
-	txt <- format(r, digits=sigfigs)
-	txt <- paste(prefix, txt, sep="")
-	
-	if(missing(cex.cor)) {
-		cex.cor <- 0.8/strwidth(txt)
-	}
-
-	text(0.5, 0.5, txt, cex = cex.cor*1.0, col=c("gray60, black")[(abs(r)>sig_threshold)+1])
+panel.cor <- function(x, y, sigfigs=2, prefix="",
+                      sig_threshold=0.5,  cex.cor, ...) {
+  # To ensure non-destructive parameter changing
+  usr <- par("usr")
+  on.exit(par(usr))
+  par <- c(0, 1, 0, 1) # Possible issue could come from an edge case here...
+  r <- cor(x, y, use = "complete.obs")
+  txt <- format(r, digits = sigfigs)
+  txt <- paste(prefix, txt, sep = "")
+  if(missing(cex.cor)) {
+    cex.cor <- 0.8 / strwidth(txt)
+  }
+  text(0.5, 0.5, txt, cex = cex.cor * 1.0,
+       col = c("gray60, black")[(abs(r) > sig_threshold) + 1])
 }
 
 # Side Effect: modifies variables not passed as arguments
 # returns: void
-panel.hist <- function(x, ...)	{
-	usr <- par("usr")
-	on.exit(par(usr))
-
-	h <- hist(x, plot=FALSE)
-	breaks <- h$breaks
-	nB <- length(breaks)
-
-	y <- h$counts
-	y <- y/max(y)
-
-	# I *think* what this does is just use the first and last break
-	# but I don't know enough about the histogram function to be
-	# sure
-	rect(breaks[-nB], 0, breaks[-1], y, col="cyan", ...)
+panel.hist <- function(x, ...) {
+  usr <- par("usr")
+  on.exit(par(usr))
+  h <- hist(x, plot = FALSE)
+  breaks <- h$breaks
+  nbrks <- length(breaks)
+  y <- h$counts
+  y <- y / max(y)
+  # I *think* what this does is just use the first and last break
+  # but I don't know enough about the histogram function to be
+  # sure
+  rect(breaks[-nbrks], 0, breaks[-1], y, col = "cyan", ...)
 }
 
 # =====END Plotting functions
@@ -104,34 +97,28 @@ panel.hist <- function(x, ...)	{
 # (also refactored slightly)
 # If R is to be believed, this is a pure function
 #    But of course R being R we can't prove that
-triple.fit <- function(X, Y, Q)	{
-	# Remove NA rows
-	idx <- sort(unique(c(
-		which(is.na(X)),
-		which(is.na(Y)),
-		which(is.na(Q))
-	)))
-	
-
-	X <- X[-idx]
-	Y <- Y[-idx]
-	Q <- Q[-idx]
-
-	# The resulting X, Y, and Qs should not be empty
-	stopifnot(length(X) > 0)
-	stopifnot(length(Y) > 0)
-	stopifnot(length(Q) > 0)
-
-	scores <- c(
-		(BIC(lm(X~Q)) + BIC(lm(Y~Q))),  # Independent X<-Q->Y
-		(BIC(lm(X~Y)) + BIC(lm(Y~Q))),  # Reactive    Q->Y->X
-		(BIC(lm(X~Q)) + BIC(lm(Y~X))),  # Causal      Q->X->Y
-		(BIC(lm(X~Q)) + BIC(lm(Y~Q+X))) # Complex
-	)
-
-	names(scores) <- c("Independent", "Reactive", "Causal", "Complex")
-
-	scores
+triple.fit <- function(X, Y, Q) {
+  # Remove NA rows
+  idx <- sort(unique(c(
+    which(is.na(X)),
+    which(is.na(Y)),
+    which(is.na(Q))
+  )))
+  X <- X[-idx]
+  Y <- Y[-idx]
+  Q <- Q[-idx]
+  # The resulting X, Y, and Qs should not be empty
+  stopifnot(length(X) > 0)
+  stopifnot(length(Y) > 0)
+  stopifnot(length(Q) > 0)
+  scores <- c(
+    (BIC(lm(X~Q)) + BIC(lm(Y~Q))),    # Independent X<-Q->Y
+    (BIC(lm(X~Y)) + BIC(lm(Y~Q))),    # Reactive    Q->Y->X
+    (BIC(lm(X~Q)) + BIC(lm(Y~X))),    # Causal      Q->X->Y
+    (BIC(lm(X~Q)) + BIC(lm(Y~Q + X))) # Complex
+  )
+  names(scores) <- c("Independent", "Reactive", "Causal", "Complex")
+  scores
 }
 
 # ======== USER SPACE ========
@@ -140,38 +127,35 @@ triple.fit <- function(X, Y, Q)	{
 # ============================
 
 pheno <- c("INS.10wk", "GLU.10wk", "TRIG.10wk")
-genes <- c("Irx3$", "Sirt1$", "Ptpn1$") # and others, there's no upper limit other than practical
+genes <- c("Irx3$", "Sirt1$", "Ptpn1$")
+# and others, there's no upper limit other than practical
 islet_expression <- lapply(genes, load_islet)
 names(islet_expression) <- genes
-
 # Non-destructive, we don't have to reload f2g$pheno if we screw up
 cross <- f2g
 cross$pheno <- prepare_for_scan(pheno, islet_expression)
-
 # 4 in 4:length(cross) is a magic number, we don't want the first 3 for pairwise scans
-pairs(cross$pheno[,4:length(cross$pheno)], upper.panel=panel.cor, diag.panel=panel.hist)
-
+pairs(cross$pheno[, 4:length(cross$pheno)],
+      upper.panel = panel.cor, diag.panel = panel.hist)
 # Sex as a covariate
 sex <- as.numeric(cross$pheno$Sex)
-
-cross_gp <- calc.genoprob(cross, step=1)
-
+cross_gp <- calc.genoprob(cross, step = 1)
 # Perform a QTL scan
-scan1 <- scanone(cross_gp, pheno.col = cat(pheno, genes), method="hk", addcovar=sex)
-
+scan1 <- scanone(cross_gp, pheno.col = cat(pheno, genes),
+                 method = "hk", addcovar = sex)
 # ID LOD significance thresholds for gene expression traits
 # 4+length(pheno) is a magic number, we need to throw out the first 3 columns and 
 # ignore clinical phenotypes
-perm1 <- scanone(cross, pheno.col = (4+length(pheno)), addcovar = sex, method="hk", n.perm=100, perm.Xsp=TRUE)
-
+perm1 <- scanone(cross, pheno.col = (4 + length(pheno)),
+         addcovar = sex, method = "hk", n.perm = 100, perm.Xsp = TRUE)
 # plot 3 rows, 1 col
-par(mfrow=c(3,1))
+par(mfrow = c(3,1))
 # Not sure what the magic number 2 is for
 # I *think* it's for removing the first 2 columns as they're useless to calculating perms
-for(i in 1:(length(scan1)-2))  {
-	plot(scan1, lodcolumn=i)
-	add.threshold(scan1, perms=perm1, alpha=0.05, lty="dashed", lwd=2, col="orange")
-	add.threshold(scan1, perms=perm1, alpha=0.10, lty="dashed", lwd=2, col="purple")
+for(i in 1:(length(scan1) - 2))  {
+  plot(scan1, lodcolumn = i)
+  add.threshold(scan1, perms = perm1, alpha = 0.05,
+                lty = "dashed", lwd = 2, col = "orange")
+  add.threshold(scan1, perms = perm1, alpha = 0.10,
+                lty = "dashed", lwd = 2, col = "purple")
 }
-
-
