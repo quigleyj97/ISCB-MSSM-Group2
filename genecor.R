3a061cd03b1b74ba96bd9fa7f96380e318fcedd4
Joe Quigley <quigleyj@mssm.org>
2016-03-17 20:20:46 -0400

    Added conditional scanning, bind() func

--- genecor.R
+++ genecor.R
@@ -1,8 +1,8 @@
-# R analysis script
+# genecor.R
+# R gene analysis script
 # Joe Quigley 2016 for ISCB 2015-16 at MSSM Group 2 with JAX Labs
 
 # depends on ggplot2 and qtl
-
 library(ggplot2)
 library(qtl)
 
@@ -32,6 +32,28 @@ load_islet <- function(gene) {
   islet.rz[, annot[gene_id, 1]]
 }
 
+# Same as load_islet but general case
+# IMPORTANT: tissue must be valid tissue
+load_expression <- function(gene, tissue) {
+  stopifnot(exists(deparse(substitute(annot))))
+  stopifnot(exists(deparse(substitute(tissue))))
+  gene_id <- grep(gene, annot$gene1)
+  stopifnot(length(gene_id) == 1)
+  tissue[, annot[gene_id, 1]]
+}
+
+# Performs a scanone conditioning for a gene
+# IMPORTANT: cross must be prepared for scanning before calling this func
+# IMPORTANT: gene must exist in the cross
+# Side effect: Accesses variables not passed as arguments
+# Returns the scan results
+scan_cond_gene <- function(pheno, gene) {
+  # Ensure the gene exists
+  stopifnot(exists(deparse(substitute(cross$pheno[gene]))))
+  scanone(cross, pheno.col=pheno, addcovar=cross$pheno[gene])
+}
+
+# Again, same, but for 
 # Prepares the f2g cross object for QTL scans by
 # removing unneeded columns and inserting clinical phenotypes
 # and gene phenotypes into the cross object. Returns the resulting
@@ -52,9 +74,16 @@ prepare_for_scan <- function(clin_pheno, gene_pheno) {
     gene_pheno)
 }
 
+# I like me some functional programming
+# This ugly hack is mostly to better utilize lapply
+# Taken from: http://r.789695.n4.nabble.com/Partial-function-application-in-R-td886216.html
+bind <- function(f, ...) {
+   args <- list(...)
+   function(...) do.call(f, c(list(...), args))
+}
+
 # =====Plotting functions
 # Taken from atg.r written by Dr. Gary Churchill and refactored slightly
-
 
 # Side Effect: modifies variables not passed as arguments
 # returns: void
@@ -63,15 +92,15 @@ panel.cor <- function(x, y, sigfigs=2, prefix="",
   # To ensure non-destructive parameter changing
   usr <- par("usr")
   on.exit(par(usr))
-  par <- c(0, 1, 0, 1) # Possible issue could come from an edge case here...
+  par(usr = c(0, 1, 0, 1)) # Possible issue could come from an edge case here...
   r <- cor(x, y, use = "complete.obs")
   txt <- format(r, digits = sigfigs)
   txt <- paste(prefix, txt, sep = "")
   if(missing(cex.cor)) {
     cex.cor <- 0.8 / strwidth(txt)
   }
-  text(0.5, 0.5, txt, cex = cex.cor * 1.0,
-       col = c("gray60, black")[(abs(r) > sig_threshold) + 1])
+  text(0.5, 0.5, txt, cex = cex.cor,
+       col = c("gray60", "black")[(abs(r) > sig_threshold) + 1])
 }
 
 # Side Effect: modifies variables not passed as arguments
@@ -121,6 +150,7 @@ triple.fit <- function(X, Y, Q) {
   scores
 }
 
+
 # ======== USER SPACE ========
 # Enter your code here: genes,
 # plotting, etc.
@@ -128,32 +158,53 @@ triple.fit <- function(X, Y, Q) {
 
 pheno <- c("INS.10wk", "GLU.10wk", "TRIG.10wk")
 genes <- c("Irx3$", "Sirt1$", "Ptpn1$")
+cond_genes <- c("Myt1l", "Cmpk2", "Cog5", "Colec11", "Efcab10", "Lpin1")
 # and others, there's no upper limit other than practical
+
 islet_expression <- lapply(genes, load_islet)
 names(islet_expression) <- genes
+
 # Non-destructive, we don't have to reload f2g$pheno if we screw up
 cross <- f2g
 cross$pheno <- prepare_for_scan(pheno, islet_expression)
+
 # 4 in 4:length(cross) is a magic number, we don't want the first 3 for pairwise scans
 pairs(cross$pheno[, 4:length(cross$pheno)],
       upper.panel = panel.cor, diag.panel = panel.hist)
+
 # Sex as a covariate
 sex <- as.numeric(cross$pheno$Sex)
 cross_gp <- calc.genoprob(cross, step = 1)
+
 # Perform a QTL scan
-scan1 <- scanone(cross_gp, pheno.col = cat(pheno, genes),
+scan1 <- scanone(cross_gp, pheno.col = c(pheno, genes),
                  method = "hk", addcovar = sex)
 # ID LOD significance thresholds for gene expression traits
 # 4+length(pheno) is a magic number, we need to throw out the first 3 columns and 
 # ignore clinical phenotypes
 perm1 <- scanone(cross, pheno.col = (4 + length(pheno)),
          addcovar = sex, method = "hk", n.perm = 100, perm.Xsp = TRUE)
+
 # plot 3 rows, 1 col
 par(mfrow = c(3,1))
 # Not sure what the magic number 2 is for
-# I *think* it's for removing the first 2 columns as they're useless to calculating perms
+# I *think* it's for removing the first 2 columns as they're not meant for plotting
+#   First two cols are chromosome and 'pos' ( I *think* peak )
 for(i in 1:(length(scan1) - 2))  {
   plot(scan1, lodcolumn = i)
+  add.threshold(scan1, perms = perm1, alpha = 0.05,
+                lty = "dashed", lwd = 2, col = "orange")
+  add.threshold(scan1, perms = perm1, alpha = 0.10,
+                lty = "dashed", lwd = 2, col = "purple")
+}
+
+# Bind our phenotypes of interest first
+cond_ins_sirt1 <- bind(cond_gene, c("INS.10wk", "Sirt1$"))
+cond_scans <- lapply(cond_genes, cond_ins_sirt1)
+
+# Magic number exists for the same reason as before
+for(i in 1:length(cond_scans) - 2)	{
+  plot(cond_scans, lodcolumn=i)
   add.threshold(scan1, perms = perm1, alpha = 0.05,
                 lty = "dashed", lwd = 2, col = "orange")
   add.threshold(scan1, perms = perm1, alpha = 0.10,
